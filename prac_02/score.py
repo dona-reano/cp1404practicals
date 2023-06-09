593296d11133c4fe95f3c2c36a2aa87576fbe858
dona-reano <donabella.reano@my.jcu.edu.au>
2023-06-09 15:49:38 +1000

    Refactor score status program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,29 +1,24 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 - Determine score status using functions
 """
 
-# Original code
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-    print("Excellent")
-if score < 50:
-    print("Bad")
 
-# Rewritten code
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get a numeric score and display its status."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
