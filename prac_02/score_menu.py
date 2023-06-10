848ef9cedb3a32cf3604428ef23f41d8198f16a3
dona-reano <donabella.reano@my.jcu.edu.au>
2023-06-10 19:18:47 +1000

    Refactor score menu program to use functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,32 +1,49 @@
 """Score menu program"""
 
-
 MENU = """G - Get a valid score (0-100)
 P - Print result
 S - Show stars
 Q - Quit"""
 
 
-"""Get a numeric score and display the same amount of asterisks."""
-score = int(input("Enter score: "))
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "G":
-        score = int(input("Enter score: "))
-    elif choice == "P":
-        if score < 0 or score > 100:
-            print("Invalid score")
-        elif score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("Passable")
-        else:
-            print("Bad")
-    elif choice == "S":
-        print("*" * score)
-    else:
-        print("Invalid option")
+def main():
+    """Get a numeric score to determine status and print using functions."""
+    score = get_valid_score()
     print(MENU)
     choice = input(">>> ").upper()
-print("Farewell.")
+    while choice != "Q":
+        if choice == "G":
+            get_valid_score()
+        elif choice == "P":
+            determine_status(score)
+        elif choice == "S":
+            print_asterisks(score)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell.")
+
+
+def get_valid_score():
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
+
+
+def determine_status(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+def print_asterisks(score):
+    print("*" * score)
+
+
+main()
