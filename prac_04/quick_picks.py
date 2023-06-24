2694d654c567a5e164021b16d24495160b77b3a3
dona-reano <donabella.reano@my.jcu.edu.au>
2023-06-24 20:32:33 +1000

    Add while loop

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,6 +10,9 @@ NUMBERS_PER_LINE = 6
 
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Cannot be less than 0.")
+        number_of_quick_picks = int(input("How many quick picks? "))
     for quick_pick in range(number_of_quick_picks):
         quick_picks = []
         for i in range(NUMBERS_PER_LINE):
