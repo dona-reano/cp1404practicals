12e3e6e01f6ec294b5643cd8d27d1f47c984c7dd
dona-reano <donabella.reano@my.jcu.edu.au>
2023-06-24 18:12:29 +1000

    Modify get_data function, add new function to display subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,12 +7,20 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    """Read subject data and display subject details."""
+    subjects = get_subjects()
+    display_subjects(subjects)
 
 
-def get_data():
+def display_subjects(subjects):
+    """Display subject details."""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
+
+
+def get_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []  # Store data in list
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +30,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subject.append(parts)  # Adds the parts to the subject list
         print("----------")
     input_file.close()
+    return subject  # Data returned as nested list
 
 
 main()
