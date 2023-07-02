54e86a6aa45c7fd55f5378584e48644825d62049
dona-reano <donabella.reano@my.jcu.edu.au>
2023-07-02 20:38:05 +1000

    Refactor main function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,12 +7,20 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
-    records = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()
-        for line in in_file:
-            elements = line.strip().split(",")
-            records.append(elements)
+    records = get_records_data()
+    champion_to_count, countries = process_data(records)
+    display_results(champion_to_count, countries)
+
+
+def display_results(champion_to_count, countries):
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in countries))
+
+
+def process_data(records):
     champion_to_count = {}
     countries = set()
     for record in records:
@@ -21,11 +29,17 @@ def main():
             champion_to_count[record[2]] += 1
         except KeyError:
             champion_to_count[record[2]] = 1
-    print("Wimbledon Champions: ")
-    for name, count in champion_to_count.items():
-        print(name, count)
-    print(f"These {len(countries)} countries have won Wimbledon: ")
-    print(", ".join(country for country in countries))
+    return champion_to_count, countries
+
+
+def get_records_data():
+    records = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            elements = line.strip().split(",")
+            records.append(elements)
+    return records
 
 
 main()
