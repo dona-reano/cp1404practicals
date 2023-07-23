4e1ab2127bf89b52a50f055866eefac0620bc550
dona-reano <donabella.reano@my.jcu.edu.au>
2023-07-23 20:01:00 +1000

    Write code to return Project object as string, and finish functions to load from and save projects to a file name

--- prac_07/project.py
+++ prac_07/project.py
@@ -13,3 +13,8 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return Project object in the form of a string"""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}" \
+               f", completion: {self.completion_percentage}%"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,9 @@ CP1404 Practical 07- Project Management Program
 Time estimated: 90 minutes
 Time taken:
 """
+from project import Project
+
+FILE_HEADER = "Name\tStart Date	Priority\tCost Estimate\tCompletion Percentage"
 FILENAME = "projects.txt"
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new " \
        "project\n- (U)pdate project\n- (Q)uit"
@@ -10,12 +13,13 @@ MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter p
 
 def main():
     print(MENU)
+    projects = []
     choice = input(">>> ").upper()
     while choice != 'Q':
         if choice == 'L':
-            print("Load")
+            projects = load_projects(projects)
         elif choice == 'S':
-            print("Save")
+            save_projects(projects)
         elif choice == 'D':
             print("Display")
         elif choice == 'F':
@@ -29,6 +33,28 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
+
+
+def save_projects(projects):
+    file_name = input("Save to file name: ")
+    out_file = open(file_name, 'w')
+    print(FILE_HEADER, file=out_file)
+    for project in projects:
+        print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate:.1f}\t"
+              f"{project.completion_percentage}", file=out_file)
+    out_file.close()
+    print(f"Saved projects to {file_name}")
+
+
+def load_projects(projects):
+    file_name = input("Load from file name: ")
+    in_file = open(file_name, 'r')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split('\t')
+        project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+        projects.append(project)
+    return projects
 
 
 main()
